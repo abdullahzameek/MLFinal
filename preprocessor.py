067cb45bd9e244e56eece63aa02fb34611f4b53b
Abdullah Zameek <arz268@nyu.edu>
2019-05-13 11:15:59 +0800

    initial commit

---
+++ preprocessor.py
@@ -0,0 +1,22 @@
+import numpy as np
+from keras.models import load_model
+
+from pyaudioclassification import feature_extraction, train, predict, print_leaderboard
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from python_speech_features import mfcc
+import scipy.io.wavfile as wav
+
+features = np.load('%s.npy' % "musicFeatures")
+labels = np.load('%s.npy' % "musicLabels")
+
+
+
+# model = train(features, labels)
+
+# model.save('train1.h5')
+# model = load_model('train1.h5')
+
+# pred = predict(model, "/home/abdullahz/Desktop/pyAudio/pyAudioClassification/gel.wav")
+# print_leaderboard(pred, "/home/abdullahz/Desktop/pyAudio/pyAudioClassification/data")
+
