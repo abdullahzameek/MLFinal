7ababaa33df00a6accb4c343732fe0242841a865
Abdullah Zameek <arz268@nyu.edu>
2019-05-14 17:03:25 +0800

    updating before major fuckery

--- preprocessor.py
+++ preprocessor.py
@@ -7,10 +7,19 @@ from matplotlib import cm
 from python_speech_features import mfcc
 import scipy.io.wavfile as wav
 
+features, labels = feature_extraction('/home/abdullahz/Desktop/pyAudio/data')
+np.save('%s.npy' % "musicFeatures", features)
+np.save('%s.npy' % "musicLabels", labels)
+
 features = np.load('%s.npy' % "musicFeatures")
 labels = np.load('%s.npy' % "musicLabels")
 
-model = load_model('train1.h5')
+model = train(features, labels)
+model.save('newModel.h5')
+model = load_model('newModel.h5')
+
+
+# model = load_model('train1.h5')
 
 pred = predict(model, "/home/abdullahz/Desktop/pyAudio/gel.wav")
 print_leaderboard(pred, "/home/abdullahz/Desktop/pyAudio/data")
